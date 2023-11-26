bf85a2376d8c6057ab1e7600f48c83eaead40078
Ralph Schuler <48420064+ralphschuler@users.noreply.github.com>
2023-11-26 01:02:16 +0100

    initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as glob from "glob"
+import * as fs from "fs"
+import * as path from "path"
 
 /**
  * The main function for the action.
@@ -7,18 +9,22 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const pattern = core.getInput('pattern');
+    const output_directory = core.getInput('output_directory') || `./${Buffer.from(new Date().toISOString()).toString('base64')}`;
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Collecting files matching '${pattern}' into '${target}'`);
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    glob(pattern, {}, (err, files) => {
+        if (err) throw err;
+        files.forEach((file, index) => {
+            const targetPath = path.resolve(output_directory, `${index}-${path.basename(file)}`);
+            fs.copyFileSync(file, targetPath);
+            core.debug(`Copied '${file}' to '${targetPath}'`);
+        });
+    });
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.debug('Collection complete.');
+    core.setOutput('output_directory', output_directory);
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
